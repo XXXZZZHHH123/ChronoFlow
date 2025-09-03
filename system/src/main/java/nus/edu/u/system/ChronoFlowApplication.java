4b5d08c3d715add1a4ab146554828cebe47ffd73
zihang <mac@zihangs-MacBook-Pro.local>
2025-09-03 17:08:34 +0800

    test

--- system/src/main/java/nus/edu/u/system/ChronoFlowApplication.java
+++ system/src/main/java/nus/edu/u/system/ChronoFlowApplication.java
@@ -1,10 +1,19 @@
 package nus.edu.u.system;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.mapping.Environment;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 
 /**
  * Start application
@@ -14,8 +23,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  */
 @MapperScan(basePackages = "nus.edu.u.system.mapper", annotationClass = Mapper.class)
 @SpringBootApplication(scanBasePackages = {"nus.edu.u.framework", "nus.edu.u.common", "nus.edu.u.system", "com.anji.captcha"})
+@Slf4j
 public class ChronoFlowApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChronoFlowApplication.class, args);
+
     }
-}
+    @Bean
+    public ApplicationRunner inspectConnections(DataSource dataSource,
+                                                RedisConnectionFactory redisConnectionFactory) {
+        return args -> {
+            // ---- 数据库连通性与元信息 ----
+            try (Connection conn = dataSource.getConnection()) {
+                DatabaseMetaData md = conn.getMetaData();
+                log.info("DB URL: {}", md.getURL());
+                log.info("DB User: {}", md.getUserName());
+                log.info("DB Product: {} {}", md.getDatabaseProductName(), md.getDatabaseProductVersion());
+                log.info("DB Driver: {} {}", md.getDriverName(), md.getDriverVersion());
+            } catch (Exception e) {
+                log.error("Failed to obtain DB connection or run connectivity check", e);
+            }
+        };
+    }}
